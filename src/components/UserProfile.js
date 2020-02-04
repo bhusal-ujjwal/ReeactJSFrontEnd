8503e4f1c26850bef14cf8006c98c3ad6b0d9a2f
bhusalujjwal <ujjwal.bhusal@outlook.com>
2020-02-04 06:31:15 +0545

    added user profile page

---
+++ src/components/UserProfile.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react'
+import Navigation from './Navigation'
+import Axios from 'axios'
+import { Form, FormGroup, Input, Button, Label, CustomInput, Container } from 'reactstrap'
+import FileUploadButton from './FileUploadButton'
+
+export default class UserProfile extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            user: null,
+            config: {
+                headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
+            },
+            selectedFile: null,
+        }
+    }
+
+    componentDidMount() {
+        Axios.get('http://localhost:3001/users/me', this.state.config)
+            .then((response) => {
+                this.setState({
+                    user: response.data
+                })
+            });
+    }
+
+    handleFileSelect = (e) => {
+        this.setState({
+            selectedFile: e.target.files[0]
+        })
+    }
+
+    uploadFile = (e) => {
+        e.preventDefault();
+        const data = new FormData()
+        data.append('myFile', this.state.selectedFile)
+        Axios.post('http://localhost:3001/upload', data, this.state.config)
+            .then((response) => {
+                this.setState({
+                    user: { ...this.state.user, image: response.data.filename }
+                })
+            }).catch((err) => console.log(err.response))
+    }
+
+    updateUser = (e) => {
+        e.preventDefault();
+        Axios.put('http://localhost:3001/users/me', this.state.user, this.state.config)
+            .then((response) => console.log(response.data)).catch((err) => console.log(err.response))
+        this.props.history.push('/dashboard');
+    }
+
+    handleChange(e) {
+        this.setState({
+            user: { ...this.state.user, [e.target.name]: e.target.value }
+        })
+    }
+
+    render() {
+        if (this.state.user === null) {
+            return <h3>Loading ...</h3>
+        } else {
+            return (
+                <div>
+                    <Navigation />
+                    <Container className='mt-4'>
+                        <Form>
+                            <FormGroup>
+                                <Label for='firstname'>First Name</Label>
+                                <Input type='text'
+                                    id="firstName"
+                                    name='firstname'
+                                    value={this.state.user.firstname}
+                                    onChange={(e) => this.handleChange(e)}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for='lastname'>Last Name</Label>
+                                <Input type='text' id='lastname'
+                                    name='lastname'
+                                    value={this.state.user.lastname}
+                                    onChange={(e) => this.handleChange(e)} />
+                            </FormGroup>
+                            <FormGroup>
+                                <img className='img-thumbnail'
+                                    width='300' src={`http://localhost:3001/uploads/${this.state.user.image}`}
+                                    alt="profile" />
+                                <CustomInput type='file' id='profilePic'
+                                    onChange={this.handleFileSelect} />
+                                {this.state.selectedFile ? (<FileUploadButton
+                                    uploadFile={this.uploadFile} />) : null}
+                            </FormGroup>
+                            <Button color='danger' onClick={this.updateUser} block>Update User</Button>
+                        </Form>
+                    </Container>
+                </div>
+            )
+        }
+    }
+}
